8640689e6cc3969ec4b4359e2c48c45cd73da237
9180 GHS Ursa Major <ghs.ursa.major@gmail.com>
2025-10-30 16:26:25 -0700

    Upload to github

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,38 @@
+package frc.robot;
+
+import ca.frc6390.athena.core.RobotBase;
+import ca.frc6390.athena.core.RobotBase.RobotBaseConfig;
+import ca.frc6390.athena.devices.EncoderConfig.EncoderType;
+import ca.frc6390.athena.devices.IMU.IMUType;
+import ca.frc6390.athena.devices.MotorController.Motor;
+import ca.frc6390.athena.drivetrains.swerve.SwerveDrivetrain;
+import ca.frc6390.athena.drivetrains.swerve.SwerveDrivetrainConfig;
+import ca.frc6390.athena.drivetrains.swerve.modules.SwerveVendorSDS;
+import edu.wpi.first.math.util.Units;
+
+public class Constants {
+
+
+        public static double[] ENCODER_OFFSETS = {0.8618,0.03125,0.2199,0.9601};
+
+        public static SwerveDrivetrainConfig DRIVETRAIN_CONFIG = SwerveDrivetrainConfig.defualt(Units.inchesToMeters(19))
+                                                    .setIMU(IMUType.WPILibADXRS450, true)
+                                                    .setDriveIds(new int[]{4,5,1,7}) //FL FR BL BR
+                                                    .setSteerIds(new int[]{6,3,2,8})
+                                                    .setEncoderIds(new int[]{12,9,11,10})
+                                                    .modules(
+                                                        SwerveVendorSDS.MK3.STANDARD.config(Motor.FALCON_500,EncoderType.CTRECANcoder).setP(0.45),
+                                                        SwerveVendorSDS.MK3.STANDARD.config(Motor.FALCON_500,EncoderType.CTRECANcoder).setP(0.45),
+                                                        SwerveVendorSDS.MK3.STANDARD.config(Motor.FALCON_500,EncoderType.CTRECANcoder).setP(0.45),
+                                                        SwerveVendorSDS.MK3.STANDARD.config(Motor.FALCON_500,EncoderType.CTRECANcoder).setP(0.45)
+                                                        ) 
+                                                    .setSteerInverted(false)  
+                                                    .setEncoderOffset(ENCODER_OFFSETS)                                                    
+                                                    .setCurrentLimit(60);
+
+
+
+        public static RobotBaseConfig<SwerveDrivetrain> robotBase = RobotBaseConfig.swerve(DRIVETRAIN_CONFIG);
+}
+
+
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import ca.frc6390.athena.controllers.EnhancedXboxController;
+import ca.frc6390.athena.core.RobotBase;
+import ca.frc6390.athena.core.RobotSendableSystem.SendableLevel;
+import ca.frc6390.athena.drivetrains.swerve.SwerveDrivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class RobotContainer {
+
+  public RobotBase<SwerveDrivetrain> robotBase = Constants.robotBase.create();
+
+  private final EnhancedXboxController driverController = new EnhancedXboxController(0)
+                                                              .setLeftInverted(false)
+                                                              .setRightInverted(true)
+                                                              .setSticksDeadzone(0.05);
+  public RobotContainer() {
+
+    robotBase.getDrivetrain().shuffleboard("DriveTrain");
+    robotBase.getDrivetrain().setDriveCommand(driverController);
+    configureBindings();
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
